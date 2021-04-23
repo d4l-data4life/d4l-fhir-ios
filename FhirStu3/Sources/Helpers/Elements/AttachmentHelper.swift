bce28a7daa80d02075e61f69d2dc3893b15ab5c7
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-04-23 12:05:07 +0200

    Add implementation only attribute to sdkutils

--- FhirStu3/Sources/Helpers/Elements/AttachmentHelper.swift
+++ FhirStu3/Sources/Helpers/Elements/AttachmentHelper.swift
@@ -14,7 +14,7 @@
 //  contact D4L by email to help@data4life.care.
 
 import Foundation
-import Data4LifeSDKUtils
+@_implementationOnly import Data4LifeSDKUtils
 
 extension Attachment {
     public static func with(title: String,
