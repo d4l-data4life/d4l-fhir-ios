878300d6bf15f5e7663343031161e7541eaf2e9d
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-04-26 17:48:45 +0200

    Updates with git url

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         .package(name: "Data4LifeSDKUtils",
                  url: "git@github.com:d4l-data4life/d4l-utils-ios.git", 
-                 .upToNextMinor(from: "0.6.0")),
+                 .upToNextMinor(from: "0.6.0")), 
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
