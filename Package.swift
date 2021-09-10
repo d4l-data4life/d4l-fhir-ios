7dd995fc2f45b85c7b0e95404040dc676a5dd016
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-09-10 11:39:00 +0200

    Add flags to build settings

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Data4LifeFHIR",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13)
     ],
     products: [
         .library(
@@ -15,26 +15,29 @@ let package = Package(
             targets: ["Data4LifeFHIR"]),
         .library(
             name: "ModelsR4",
-            targets: ["ModelsR4"]),
+            targets: ["ModelsR4"])
     ],
     dependencies: [
         .package(name: "Data4LifeSDKUtils",
                  url: "https://github.com/d4l-data4life/d4l-utils-ios.git",
-                 .upToNextMinor(from: "0.6.0")),
+                 .upToNextMinor(from: "0.6.0"))
     ],
     targets: [
         .binaryTarget(
             name: "Data4LifeFHIRCore",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-fhir-ios/Data4LifeFHIRCore-xcframework-update-release-script.zip",
-            checksum: "58949559b5e79f740caaeacb84d2c0e988c12106d152a14df11b41cec88eebf6"),
+            checksum: "58949559b5e79f740caaeacb84d2c0e988c12106d152a14df11b41cec88eebf6"
+        ),
         .binaryTarget(
             name: "Data4LifeFHIR",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-fhir-ios/Data4LifeFHIR-xcframework-update-release-script.zip",
-            checksum: "1f6421725a6e406ac51405df678c2974b4379bdccccc377ce7888a783a2d10f6"),
+            checksum: "1f6421725a6e406ac51405df678c2974b4379bdccccc377ce7888a783a2d10f6"
+        ),
         .binaryTarget(
             name: "ModelsR4",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-fhir-ios/ModelsR4-xcframework-update-release-script.zip",
-            checksum: "fa3e8a211e0cd0380b29b1c72b6ac5d845849e1104b422c57008b1739dff1f55"),
+            checksum: "fa3e8a211e0cd0380b29b1c72b6ac5d845849e1104b422c57008b1739dff1f55"
+        ),
         .target(name: "Data4LifeSDKUtilsFHIR",
                 dependencies: [
                     .product(name: "Data4LifeSDKUtils",
@@ -53,7 +56,7 @@ let package = Package(
                 dependencies: [
                     .target(name: "Data4LifeSDKUtilsFHIR"),
                     .target(name: "ModelsR4"),
-                    .target(name: "Data4LifeFHIRCore"),
+                    .target(name: "Data4LifeFHIRCore")
                 ],
                 path: "SwiftPMDummyTargets/ModelsR4AllFrameworks"),
         .testTarget(name: "Data4LifeFhirTests",
