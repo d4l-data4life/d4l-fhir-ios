a2af33d84ba7fa3c2209a5877908f65a8ff9d9a8
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-09-10 11:08:06 +0200

    Preparing for the update-release-script release

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-
 let package = Package(
     name: "Data4LifeFHIR",
     platforms: [
@@ -25,22 +23,20 @@ let package = Package(
                  .upToNextMinor(from: "0.6.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Data4LifeFHIRCore",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-fhir-ios/Data4LifeFHIRCore-xcframework-update-release-script.zip",
-            checksum: "89952c1ab774130a5e93ce09de132c35fecd1f4f5e0e0399ba955f24911503ee"
+            checksum: "58949559b5e79f740caaeacb84d2c0e988c12106d152a14df11b41cec88eebf6",
         ),
         .binaryTarget(
             name: "Data4LifeFHIR",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-fhir-ios/Data4LifeFHIR-xcframework-update-release-script.zip",
-            checksum: "11cb2d70b564e9f5b12dca49192d070e3f991c78d3c83685fdadc98f00ee0a85"
+            checksum: "1f6421725a6e406ac51405df678c2974b4379bdccccc377ce7888a783a2d10f6",
         ),
         .binaryTarget(
             name: "ModelsR4",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-fhir-ios/ModelsR4-xcframework-update-release-script.zip",
-            checksum: "8cfab47f85e1746c3cb0a5ccbc35f8ea95be820558e89936e7a341401d9d205c"
+            checksum: "fa3e8a211e0cd0380b29b1c72b6ac5d845849e1104b422c57008b1739dff1f55",
         ),
         .target(name: "Data4LifeSDKUtilsFHIR",
                 dependencies: [
